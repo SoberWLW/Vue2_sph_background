395c23a5a3feda7f09e6c899135e520ba80e43df
SoberWLW <1176458601@qq.com>
2022-10-24 16:30:26 +0800

    123

---
+++ 后台vue2完整/src/views/dashboard/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="dashboard">
+    <TopView />
+    <MiddleView />
+    <BottomView />
+  </div>
+</template>
+
+<script>
+import TopView from './components/TopView.vue'
+import MiddleView from './components/MiddleView.vue'
+import BottomView from './components/BottomView.vue'
+export default {
+  name: 'Dashboard',
+  components: {
+    TopView,
+    MiddleView,
+    BottomView
+  },
+  // 页面加载后,先获取mock的数据
+  mounted() {
+    this.$store.dispatch('data/getReportData')
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  width: 100%;
+  padding: 20px;
+  background: #eee;
+}
+</style>
